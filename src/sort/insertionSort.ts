8f1b0b843eb1bec03a9a82a8015e68fe84c43875
ryo togashi <ryo.togashi.ca@gmail.com>
2019-01-25 09:49:34 -0800

    Change directory structure

---
+++ src/sort/insertionSort.ts
@@ -0,0 +1,19 @@
+function InsertionSort(alist) {
+  for (var i = 1; i <= alist.length - 1; i++) {
+    for (var j = i - 1; 0 <= j; j--) {
+      if (alist[i] < alist[j]) {
+        var temp = alist[i];
+        alist[i] = alist[j];
+        alist[j] = temp;
+        i--
+        console.log(alist)
+      }else{
+        break
+      }
+    }
+  }
+}
+
+var alist: Array<number> = [91, 921, 1000, 5, 2, 6, 9, 4, 9, 12, 24, 46, 68];
+console.log(alist)
+InsertionSort(alist);
