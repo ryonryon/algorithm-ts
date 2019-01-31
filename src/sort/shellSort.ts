6114f4defaaed83cb2425a7b5b9b5e15734af4e3
ryo togashi <ryo.togashi.ca@gmail.com>
2019-01-30 18:12:51 -0800

    modified shellSort.ts

--- src/sort/shellSort.ts
+++ src/sort/shellSort.ts
@@ -1,32 +1,31 @@
-function ShellSort(alist) {
-    for (var gap = Math.floor(alist.length / 2) - 1;1 <= gap;gap = Math.floor(gap / 2)) {
-        
-        for(var i = 0;alist.length > i + gap;i++){
+import { OnSwap } from "./event";
+import { swap } from "./utility";
 
-            if(alist[i + gap] < alist[i]){
-
-                var temp = alist[i];
-                alist[i] = alist[i + gap];
-                alist[i + gap] = temp;
-                console.log(alist);
-            }
-            for(var j = i; 0 <= j - gap; j = j - gap){
-
-                if(alist[j] < alist[j-gap]){
-
-                    var temp = alist[j-gap];
-                    alist[j-gap] = alist[j];
-                    alist[j] = temp;
-                    console.log(alist);
-                }else{
-                    break
-                }
-            }
+function ShellSort<Value>({
+    array,
+    OnSwap = () => {}
+  }: {
+    array: Value[];
+    OnSwap?: OnSwap<Value>;
+  }): void {
+  for (
+    var gap = Math.floor(array.length / 2) - 1;
+    1 <= gap;
+    gap = Math.floor(gap / 2)
+  ) {
+    for (var i = 0; array.length > i + gap; i++) {
+      if (array[i + gap] < array[i]) {
+        swap(array,i,i + gap,OnSwap);
+      }
+      for (var j = i; 0 <= j - gap; j = j - gap) {
+        if (array[j] < array[j - gap]) {
+          swap(array,j,j - gap,OnSwap);
+        } else {
+          break;
         }
+      }
     }
+  }
 }
 
-var alist: Array<number> = [5, 79, 6, 9, 4, 12, 24, 46, 68, 1, 43, 199, 5000, 533, 3, 9804, 3];
-console.log(alist.length);
-console.log(alist);
-ShellSort(alist);
+export default ShellSort;
